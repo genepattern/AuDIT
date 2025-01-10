db38cef77a89a04f5f5f55eb8690005239ebd51c
liefeld <liefeld@broadinstitute.org>
2025-01-10 14:02:31 -0800

    Adding explicit exit from the run script due to some jobs hanging around after it ends

--- src/run_audit.sh
+++ src/run_audit.sh
@@ -3,3 +3,7 @@
 # to avoid mucking about with escaping args to java
 
 java -Dr_flags="--no-save --quiet --slave --no-restore"  -DR_HOME=/usr/local/lib64/R -cp /build RunR /build/audit/AuDIT.R parse.cmdline /build/audit/ "$@"
+java_exit=$?
+echo "AuDIT finished" 
+exit   $java_exit
+
